14a958fde9dff0ba18a7cf2c5696d8f028f86424
CamBach <camille.bachmann.2005@gmail.com>
2024-07-23 17:58:20 +0200

    CODE DIALOGUE QUI MARCHEEEE

--- smash or smash/Assets/DialogScript.cs
+++ smash or smash/Assets/DialogScript.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class DialogScript : MonoBehaviour
 {
     [SerializeField] GameObject nextDialog;
-
+    bool isReady = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShowNextDialog();
+        }
+    }
     public void ShowNextDialog()
     {
-        nextDialog.SetActive(true);
+        nextDialog.GetComponent<Canvas>().enabled = true;
+        nextDialog.GetComponent<DialogScript>().enabled = true;
         gameObject.SetActive(false);
     }
 }
