955d65458ac35c09cbb223e7647ed6af81a48e0d
CamBach <camille.bachmann.2005@gmail.com>
2024-07-26 11:11:22 +0200

    Crewmate+LiaisonBoris-Crewmate

--- smash or smash/Assets/Script/Challenger.cs
+++ smash or smash/Assets/Script/Challenger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.TextCore.Text;
+using UnityEditor.SceneManagement;
 
 public class Challenger : MonoBehaviour
 {
@@ -14,6 +15,7 @@ public class Challenger : MonoBehaviour
     [SerializeField] int vie;
     [SerializeField] int mental;
     [SerializeField] int romance;
+    [SerializeField] private string nextfight;
     public Character character;
     // Start is called before the first frame update
     void Awake()
@@ -35,6 +37,10 @@ public class Challenger : MonoBehaviour
     private void Update()
     {
         vie = character.vie;
+        if (isBoris && character.Vie<=0 || character.Mental<=0)
+        {
+            LoadScenenextfight();
+        }
     }
 
     public void SetLife(int damage)
@@ -48,6 +54,10 @@ public class Challenger : MonoBehaviour
         if (isBoris && character.Vie>=50) {
             character.Mental -= emotionaldamage/2;
         }
+        if(isCrewmate && character.Vie <= 75)
+        {
+            character.Mental -= emotionaldamage*2;
+        }
         else {
             character.Mental -= emotionaldamage;
             eventSystem.SetSelectedGameObject(null);
@@ -60,10 +70,19 @@ public class Challenger : MonoBehaviour
         if (isBoris && character.Vie<=50) {
             character.Romance += love*2;
         }
+        if (isCrewmate && character.Vie <= 75)
+        {
+            character.Romance += love* 2;
+        }
         else {
             character.Romance += love;
             eventSystem.SetSelectedGameObject(null);
         }
         
+    }
+
+    void LoadScenenextfight()
+    {
+        EditorSceneManager.LoadScene(nextfight);
     }
 }
